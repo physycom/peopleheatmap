11a3dd5ded75d01a12878f072408fc3761f1a7aa
Stefano Sinigardi <stesinigardi@hotmail.com>
2018-05-29 23:02:00 +0200

    add specific commands for osmino, fix fltk include path

--- build.sh
+++ build.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+if [[ $(hostname) == *"osmino"* ]]; then
+  export FLTK_DIR=/disk01/fltk/fltk-1.3.4
+  export FLTK_INCLUDE_DIR=/disk01/fltk/fltk-1.3.4/include
+  export FLTK_BIN_DIR=/disk01/fltk/fltk-1.3.4/bin
+  export FLTK_LIBRARY_SEARCH_PATH=/disk01/fltk/fltk-1.3.4/lib
+  export FLTK_LIBRARY_DIR=/disk01/fltk/fltk-1.3.4/lib
+fi
+
 if [[ "$OSTYPE" == "darwin"* && "$1" == "gcc" ]]; then
-  export CC="/usr/local/bin/gcc-7"
-  export CXX="/usr/local/bin/g++-7"
+  export CC="/usr/local/bin/gcc-8"
+  export CXX="/usr/local/bin/g++-8"
 fi
 
 mkdir -p build
 cd build
-cmake ..
+cmake .. -DOVERRIDE_FLTK_INSTALL_DIR:BOOL=TRUE
 cmake --build . --target install
 cd ..
